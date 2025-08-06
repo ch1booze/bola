4827e1b50f6d447982b4a13f11b07da2f08a259c
Victor Momodu <victoramomodu@gmail.com>
2025-08-06 15:35:59 +0100

    feat: implement preference and caregive routes

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
+import uuid
 from datetime import date
-from enum import Enum
+from enum import StrEnum
 from typing import Optional
 
 from sqlalchemy.dialects.postgresql import JSON
@@ -14,12 +15,37 @@ class CreateUserForm(SQLModel):
 
 
 class User(CreateUserForm, table=True):
-    id: Optional[int] = Field(default=None, primary_key=True)
+    id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
+
+
+class SpeechPreference(StrEnum):
+    CASUAL = "casual"
+    NEUTRAL = "neutral"
+    RESPECTFUL = "respectful"
+
+
+class SetNicknameForm(SQLModel):
+    nickname: str
+
+
+class SetInterestsForm(SQLModel):
+    interests: list[str]
+
+
+class UserPreferences(SQLModel, table=True):
+    user_id: uuid.UUID = Field(foreign_key="user.id", primary_key=True)
     nickname: Optional[str] = None
-
-
-class UserInterest(SQLModel, table=True):
-    user_id: Optional[int] = Field(
-        default=None, foreign_key="user.id", primary_key=True
-    )
     interests: dict = Field(sa_column=Column(JSON), default_factory=list)
+    speech_preference: SpeechPreference
+
+
+class CreateCaregiverForm(SQLModel):
+    user_id: uuid.UUID = Field(foreign_key="user.id")
+    first_name: str
+    last_name: str
+    relationship_to_user: str
+    contact: str
+
+
+class Caregiver(CreateCaregiverForm, table=True):
+    id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
